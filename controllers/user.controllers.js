042ebf99746394eaba204942ccf0f22db68ee4e4
thoanguyen2907 <thoanguyen290795@gmail.com>
2021-06-06 22:20:52 +0300

    create user database

---
+++ controllers/user.controllers.js
@@ -0,0 +1,18 @@
+
+const {User} = require("../models"); 
+
+const register = async (req, res) => {
+    const {name, email, password, numberPhone} = req.body; 
+
+    try {
+        const newUser = await User.create({ name, email, password, numberPhone, type: "Client"});
+        res.status(201).send(newUser); 
+
+    } catch(error) {
+        res.status(500).send(error); 
+    }
+}
+
+module.exports = {
+    register
+}
