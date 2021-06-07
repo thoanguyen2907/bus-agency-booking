8e564754fbcce34bd3bc0bac927204b74a4db159
thoanguyen2907 <thoanguyen290795@gmail.com>
2021-06-07 22:30:38 +0300

    create login feature

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -13,6 +13,35 @@ const register = async (req, res) => {
     }
 }
 
+const login = async (req, res) => {
+    const {email, password} = req.body; 
+
+    try {
+        //kiếm ra user
+        const user = await User.findOne({
+            where: {
+                email
+            }
+        }); 
+        if(user){
+//kiểm tra mật khẩu đúng hay ko
+const isAuth = bcrypt.compareSync(password, user.password); 
+if(isAuth){
+    res.status(200).send({message: "Đăng nhập thành công"})
+} else {
+    res.status(500).send({message: "Tài khoản không hợp lệ"}); 
+
+}
+        } else {
+            res.status(500).send({message: "User không tồn tại"}); 
+        }
+        
+    } catch(error) {
+        res.status(500).send(error); 
+    }
+}
+
 module.exports = {
-    register
+    register, 
+    login
 }
