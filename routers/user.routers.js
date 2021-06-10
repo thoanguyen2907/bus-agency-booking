b75612d4bc812ea1a7594878d72c4a09bfd75808
thoanguyen2907 <thoanguyen290795@gmail.com>
2021-06-10 22:23:21 +0300

    save path of file upload

--- routers/user.routers.js
+++ routers/user.routers.js
@@ -1,17 +1,14 @@
 const  express = require("express");
-const { register, login } = require("../controllers/user.controllers");
-
-const multer  = require('multer')
-const upload = multer({ dest: 'uploads/avatar' })
+const { register, login, uploadAvatar } = require("../controllers/user.controllers");
+const { authenticate } = require("../middlewares/auth/authenticate");
+const { uploadImage } = require("../middlewares/upload/upload-image");
 
 const userRouter = express.Router()
 
 userRouter.post("/register", register); 
 userRouter.post("/login", login); 
 
-userRouter.post("/upload-avatar", upload.single('avatar'), (req, res) => {
-  res.send("Upload file Run Already !!"); 
-})
+userRouter.post("/upload-avatar", authenticate, uploadImage(), uploadAvatar)
 
 module.exports = {
   userRouter
