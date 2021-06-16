1ef9347491c62b07d89ba71af43f1601e3f7f92f
thoanguyen2907 <thoanguyen290795@gmail.com>
2021-06-17 00:59:25 +0300

    add data to seed

--- seeders/20210616213208-create-fake-ticket.js
+++ seeders/20210616213208-create-fake-ticket.js
@@ -11,6 +11,29 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
     */
+
+     await queryInterface.bulkInsert('tickets', [
+       {
+      trip_id: 1,
+      user_id: 1,
+      createdAt: "2021-05-13 8:30:00",  
+    updateAt: "2021-05-14 8:30:00"
+     },
+
+     {
+      trip_id: 2,
+      user_id: 1,
+      createdAt: "2021-05-13 8:30:00",  
+    updateAt: "2021-05-14 8:30:00"
+     },
+
+     {
+      trip_id: 3,
+      user_id: 2,
+      createdAt: "2021-05-13 8:30:00",  
+    updateAt: "2021-05-14 8:30:00"
+     }
+    ], {});
   },
 
   down: async (queryInterface, Sequelize) => {
--- seeders/20210616213214-create-fake-user.js
+++ seeders/20210616213214-create-fake-user.js
@@ -12,14 +12,50 @@ module.exports = {
      * }], {});
     */
 
-      await queryInterface.bulkInsert('users', [{
+      await queryInterface.bulkInsert('users', [
+        {
        name: 'John Doe',
        email: 'johndoe@gmail.com', 
        password: '123123',
       phone: '123123',
       avatar: 'https://cdn.halloota.com/2020/04/husky-koomiset-kuvat.jpg', 
-      type: 'ADMIN'
-     }], {});
+      type: 'ADMIN',
+      createdAt: "2021-05-13 8:30:00",  
+     updateAt: "2021-05-14 8:30:00"
+     },
+
+     {
+      name: 'Thoa',
+      email: 'thoang@gmail.com', 
+      password: '123123',
+     phone: '123123',
+     avatar: 'https://cdn.halloota.com/2020/04/husky-koomiset-kuvat.jpg', 
+     type: 'ADMIN',
+     createdAt: "2021-05-13 8:30:00",  
+    updateAt: "2021-05-14 8:30:00"
+    }, 
+    {
+      name: 'Tâm',
+      email: 'tamng@gmail.com', 
+      password: '123123',
+     phone: '123123',
+     avatar: 'https://cdn.halloota.com/2020/04/husky-koomiset-kuvat.jpg', 
+     type: 'ADMIN',
+     createdAt: "2021-05-13 8:30:00",  
+    updateAt: "2021-05-14 8:30:00"
+    },
+    {
+      name: 'Trúc',
+      email: 'trucng@gmail.com', 
+      password: '123123',
+     phone: '123123',
+     avatar: 'https://cdn.halloota.com/2020/04/husky-koomiset-kuvat.jpg', 
+     type: 'ADMIN',
+     createdAt: "2021-05-13 8:30:00",  
+    updateAt: "2021-05-14 8:30:00"
+    }
+
+    ], {});
   },
 
   down: async (queryInterface, Sequelize) => {
